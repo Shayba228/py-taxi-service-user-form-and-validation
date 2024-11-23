777bdb5c92861b58258d3fce8430bb97140ce5c6
Bohdan Bastiuk <bbastyuk@gmail.com>
2024-11-23 01:16:42 +0100

    Solution fix flake8

--- taxi/forms.py
+++ taxi/forms.py
@@ -5,10 +5,12 @@ from .models import Driver, Car
 
 def validate_license_number(license_number):
     if len(license_number) != 8:
-        raise forms.ValidationError("License number must be exactly 8 characters long.")
+        raise forms.ValidationError("License number must"
+                                    " be exactly 8 characters long.")
 
     if not license_number[:3].isalpha() or not license_number[:3].isupper():
-        raise forms.ValidationError("The first 3 characters must be uppercase letters.")
+        raise forms.ValidationError("The first 3 "
+                                    "characters must be uppercase letters.")
 
     if not license_number[3:].isdigit():
         raise forms.ValidationError("The last 5 characters must be digits.")
